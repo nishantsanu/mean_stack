dd54965f6477532746ebcc97451978aef4c424cc
unknown <nkumarnet40@gmail.com>
2019-07-12 03:01:00 +0530

    add delete post action

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,31 @@ module.exports.create = function(req,res){
     });
 
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        //.id means converting the object id into string
+        if(comment.user == req.user.id){
+           comment.remove();
+           Comment.deleteMany({comment: req.params.id},function(err){
+               return res.redirect('back');
+           });
+       }else{
+         return res.redirect('back');
+       }
+
+//     Post.findById(comment,function(err,post){
+
+//         if(post.user == req.user.id){
+//             comment.remove();
+//             Comment.deleteMany({comment: req.params.id},function(err){
+//                 return res.redirect('back');
+//         });
+// }
+//     });
+    
+       
+}
+    
+);
+}
