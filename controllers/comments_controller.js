e51ccbd25e931d0378c0e476dfc36efaf27fd56e
unknown <nkumarnet40@gmail.com>
2019-07-12 05:08:10 +0530

    add delete comment action only for current user

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -27,26 +27,60 @@ module.exports.destroy = function(req,res){
     Comment.findById(req.params.id,function(err,comment){
         //.id means converting the object id into string
         if(comment.user == req.user.id){
+            //save its post id
+            let postId = comment.post;  
            comment.remove();
-           Comment.deleteMany({comment: req.params.id},function(err){
+           Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
                return res.redirect('back');
-           });
-       }else{
-         return res.redirect('back');
+           })   
+       }
+       else{
+         return res.redirect ('back');
        }
 
-//     Post.findById(comment,function(err,post){
-
-//         if(post.user == req.user.id){
-//             comment.remove();
-//             Comment.deleteMany({comment: req.params.id},function(err){
-//                 return res.redirect('back');
-//         });
-// }
-//     });
-    
        
 }
     
 );
 }
+
+// module.exports.destroy = function(req,res){
+//     Comment.findById(req.params.id,function(err,comment){
+//         //.id means converting the object id into string
+        
+//         if(comment.user == req.user.id){
+//             //save its post id
+//             let postId = comment.post;  
+//            comment.remove();
+//            Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
+//                console.log('1');
+//                return res.redirect('back');
+//            })
+//            return res.redirect('back');
+//            console.log('2');
+//        }
+//        Post.findById(comment.post,function(err,post){
+//         //.id means converting the object id into string
+//         console.log('3');
+//         if(post.user == req.user.id){
+//             //save its post id
+//             let postId = comment.post;  
+//            comment.remove();
+//            Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
+//                return res.redirect('back');
+//            })
+          
+//        }
+//        else{
+//            return res.redirect('back');
+//        }
+       
+
+
+// });
+// console.log('4');
+
+// return res.redirect('back');
+
+// });
+// }
--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,9 @@ const passport=require('passport');
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create',passport.checkAuthentication, commentsController.create);
+// router.get('/destroy/:cid&:pid',passport.checkAuthentication,commentsController.destroy);
 router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
+
 
 
 module.exports = router;
