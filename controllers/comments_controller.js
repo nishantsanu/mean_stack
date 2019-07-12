da95a43569a5f8a2b0c5a5d070f5d271f358e713
unknown <nkumarnet40@gmail.com>
2019-07-13 01:37:38 +0530

    add comment delete option for owner of post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -23,64 +23,22 @@ module.exports.create = function(req,res){
 
 }
 
+
 module.exports.destroy = function(req,res){
     Comment.findById(req.params.id,function(err,comment){
         //.id means converting the object id into string
-        if(comment.user == req.user.id){
-            //save its post id
-            let postId = comment.post;  
-           comment.remove();
-           Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
-               return res.redirect('back');
-           })   
-       }
-       else{
-         return res.redirect ('back');
-       }
+        Post.findById(comment.post,function(err,post){
 
-       
+            if(comment.user == req.user.id||post.user==req.user.id){ 
+               comment.remove();
+               Post.findByIdAndUpdate(post,{ $pull: {comments: req.param.id}},function(err,post){
+                   
+               })
+           }
+        });
+});
+
+
+return res.redirect('back');
+
 }
-    
-);
-}
-
-// module.exports.destroy = function(req,res){
-//     Comment.findById(req.params.id,function(err,comment){
-//         //.id means converting the object id into string
-        
-//         if(comment.user == req.user.id){
-//             //save its post id
-//             let postId = comment.post;  
-//            comment.remove();
-//            Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
-//                console.log('1');
-//                return res.redirect('back');
-//            })
-//            return res.redirect('back');
-//            console.log('2');
-//        }
-//        Post.findById(comment.post,function(err,post){
-//         //.id means converting the object id into string
-//         console.log('3');
-//         if(post.user == req.user.id){
-//             //save its post id
-//             let postId = comment.post;  
-//            comment.remove();
-//            Post.findByIdAndUpdate(postId,{ $pull: {comments: req.param.id}},function(err,post){
-//                return res.redirect('back');
-//            })
-          
-//        }
-//        else{
-//            return res.redirect('back');
-//        }
-       
-
-
-// });
-// console.log('4');
-
-// return res.redirect('back');
-
-// });
-// }
