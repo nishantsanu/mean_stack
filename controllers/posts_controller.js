1d81a85fb73f74e4c22a8dfa6bc3a9c168b2d118
unknown <nkumarnet40@gmail.com>
2019-07-13 17:01:56 +0530

    creat a post using AJAX : sending data part

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,7 +7,16 @@ module.exports.create = async function(req,res){
         let post= await Post.create({
             content : req.body.content,
             user:req.user._id,
-        })
+        });
+//handle by ajax
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
         req.flash('success','Post published!');
           return res.redirect('back');
     } catch (err) {
